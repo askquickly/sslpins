cc8b3ebf17e18fe3aaed0019cfec177464a5c745
J <askquickly@users.noreply.github.com>
2018-01-27 21:58:24 +0000

    Update sslpins.java
    Added Json Post

--- sslpins.java
+++ sslpins.java
@@ -22,7 +22,7 @@ import anywheresoftware.b4a.BA.Author;
 import anywheresoftware.b4a.keywords.Common;
 
 @Author("AskQuickly")
-@Version(1.0f)
+@Version(1.1f)
 @Permissions(values = {"android.permission.INTERNET"})
 @DependsOn(values = {"okhttp-3.9.1","okio-1.13.0"})
 @ShortName("SSLPins")
@@ -59,6 +59,25 @@ public class SSLPins {
   }
  }
  OkHttpClient client = new OkHttpClient();
+ String doPostRequest(String url, String json) throws IOException {
+ CertificatePinner certificatePinner = new CertificatePinner.Builder()
+  .add(url, key)
+  .build();
+
+  OkHttpClient client = new OkHttpClient.Builder()
+   .certificatePinner(certificatePinner)
+   .build();
+
+	MediaType type = MediaType.parse("application/json; charset=utf-8");
+    RequestBody body = RequestBody.create(type, json);
+    Request request = new Request.Builder()
+    .url(url)
+    .post(body)
+    .build();
+	
+    Response response = client.newCall(request).execute();
+    return response.body().string();
+      }
  String doGetRequest(String url) throws IOException {
 
   CertificatePinner certificatePinner = new CertificatePinner.Builder()
@@ -87,6 +106,19 @@ public class SSLPins {
    }
   } catch (IOException e) {
    // BA.Log(e.getMessage());
+   ba.raiseEventFromUI(this, SSLPins.eventName + "_ready", e.getMessage());
+  }
+ }
+ public void PostJsonRequest(String Ia, String Json) throws IOException {
+  try {
+   SSLPins example = new SSLPins();
+   String getResponse = example.doPostRequest(Ia, Json);
+   if (ba.subExists(SSLPins.eventName + "_ready")) {
+    ba.raiseEventFromUI(this, SSLPins.eventName + "_ready", getResponse);
+   } else {
+    BA.LogError("event sub does not exist: " + SSLPins.eventName);
+   }
+  } catch (IOException e) {
    ba.raiseEventFromUI(this, SSLPins.eventName + "_ready", e.getMessage());
   }
  }
