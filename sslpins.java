d6e99c6d85c2cc53411f1c860b938ddb25638df8
J <askquickly@users.noreply.github.com>
2018-01-27 10:35:34 +0000

    Update sslpins.java

--- sslpins.java
+++ sslpins.java
@@ -1,6 +1,19 @@
 package sslpins;
 
 import anywheresoftware.b4a.BA;
-import anywheresoftware.ba.*;
+import anywheresoftware.b4a.BA.ShortName;
+import anywheresoftware.b4a.BA.Version;
+import anywheresoftware.b4a.BA.Author;
+import anywheresoftware.b4a.BA.Hide;
+import anywheresoftware.b4a.BA.Events;
 
-@shortname("
+@Author("SSLPins")
+@Version(0.01f)
+@ShortName("SSLPins")
+
+public class SSLPins{
+  
+  public String doCheck(String url, String key){
+  // build checks here from certificatePinner
+  }
+}
