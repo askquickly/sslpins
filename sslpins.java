6f9e4ddc2a64b40acf02909d57d00c47cfb55052
J <askquickly@users.noreply.github.com>
2018-01-28 09:56:21 +0000

    Update sslpins.java

--- sslpins.java
+++ sslpins.java
@@ -7,7 +7,6 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Headers;
-import java.io.IOException;
 import okhttp3.Response;
 import okhttp3.CertificatePinner;
 
